7086ee585df243857f34cb0f512974cc775c22f1
Jamie Sadler <jamietsadler@hotmail.co.uk>
2021-08-05 19:28:08 +0100

    added toolkit templates

--- DTK/toolkit/views.py
+++ DTK/toolkit/views.py
@@ -14,6 +14,21 @@ from flask_login import login_required, login_user, logout_user
 blueprint = Blueprint("toolkit", __name__, url_prefix="/toolkit", static_folder="../static")
 
 @blueprint.route("/")
-def members():
+def dtkhome():
     """List members."""
     return render_template("dtkbase.html")
+
+@blueprint.route("/dashboard")
+def toolkitdashboard():
+    """List members."""
+    return render_template("toolkit/dashboard.html")
+
+@blueprint.route("/")
+def connect():
+    """List members."""
+    return render_template("toolkit/connect.html")
+
+@blueprint.route("/workflow")
+def workflow():
+    """List members."""
+    return render_template("toolkit/flow.html")
